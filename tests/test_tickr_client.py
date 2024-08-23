2c8b52a7a2e1b5b8ef4d379582b3cea82da67b42
syncsoftco <syncboard@googlegroups.com>
2024-08-22 22:22:24 -0400

    Update test_tickr_client.py

--- tests/test_tickr_client.py
+++ tests/test_tickr_client.py
@@ -1,18 +1,18 @@
 """
 test_tickr_client.py
 
-Unit tests for the TickrClient class in tickr_client.py.
+Unit tests for the TickrClient class in tickr/tickr_client.py.
 
 License: MIT
 """
 
 import unittest
 from unittest.mock import patch, MagicMock
-from tickr.tickr_client import TickrClient
+from tickr.ticker_client import TickrClient
 from datetime import datetime, timedelta
 
 class TestTickrClient(unittest.TestCase):
-    @patch('tickr_client.Github')
+    @patch('tickr.ticker_client.Github')
     def setUp(self, MockGithub):
         """
         Set up the TickrClient for testing with mocked GitHub API responses.
