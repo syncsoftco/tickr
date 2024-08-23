c519b0e875a7c9f5b321aebacc3c7b4241859c9d
syncsoftco <syncboard@googlegroups.com>
2024-08-22 22:16:35 -0400

    Update and rename requirements.txt to setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tickr',
+    version='1.0.0',
+    description='A high-level API client for accessing cryptocurrency candle data from a GitHub repository.',
+    author='Patrick',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'PyGithub>=1.55',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
